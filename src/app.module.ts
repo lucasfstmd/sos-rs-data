0883b995385ba505e4d1a020cafc23e704bccd51
Lucas Fausto <lucasfstmd.projetos@gmail.com>
2024-05-10 12:34:26 -0300

    Activity Done:  - Added .env.example  - Changes in docker-compose  - Cganges in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SosModule } from './sos/sos.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'process';
 
 @Module({
-  imports: [SosModule, MongooseModule.forRoot('mongodb://mongo:27017/sos-rs')],
+  imports: [SosModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
